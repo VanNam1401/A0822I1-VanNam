f76473bc7d3abc1e30aa849d0b748d8cf55ed054
Your Name <you@example.com>
2022-10-06 08:20:12 +0700

    06/10/2022

--- module1/ss13_array_2/bai_tap/array_available/algorithm.js
+++ module1/ss13_array_2/bai_tap/array_available/algorithm.js
@@ -1,19 +1,33 @@
+function practice1() {
+    let num;
+}
+
+function practice2() {
+    let num = prompt("Nhap mot so: ");
+    let str = num.toString();
+    let result = [str[0]];
+    for (let i = 1; i < str.length; i++) {
+        if ((str[i - 1] % 2 === 0) && (str[i] % 2 === 0)) {
+            result.push('-', str[i]);
+        } else {
+            result.push(str[i]);
+        }
+    }
+    console.log(result.join(''));
+}
+
 function practice3() {
     let string = prompt("Nhập chuỗi ");
     let str = string.toString();
     let UPPER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     let LOWER = 'abcdefghijklmnopqrstuvwxyz';
     let ketQua = [];
-    for ( let i = 0 ; i < str.length ; i++){
-        if (UPPER.indexOf(str[i]) !== -1)
-        {
+    for (let i = 0; i < str.length; i++) {
+        if (UPPER.indexOf(str[i]) !== -1) {
             ketQua.push(str[i].toLowerCase())
-        }
-        else if (LOWER.indexOf(str[i]) !== -1)
-        {
+        } else if (LOWER.indexOf(str[i]) !== -1) {
             ketQua.push(str[i].toUpperCase())
-        }else
-        {
+        } else {
             ketQua.push(str[i]);
         }
 
