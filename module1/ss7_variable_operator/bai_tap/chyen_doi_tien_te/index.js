942b4cd39e6814cb41d08cc980e673fa6fedd813
Your Name <you@example.com>
2022-10-25 18:13:24 +0700

    update date 25.10

--- module1/ss7_variable_operator/bai_tap/chyen_doi_tien_te/index.js
+++ module1/ss7_variable_operator/bai_tap/chyen_doi_tien_te/index.js
@@ -5,10 +5,10 @@ function convert() {
     if (chon1 == chon2) {
         document.getElementById("result").innerHTML = "Result:  " + money;
     }
-    if (chon1 == "VN" && chon2 == "usd"){
+    if (chon1 == "VN" && chon2 == "usd") {
         document.getElementById("result").innerHTML = "Result: " + money / 23000;
     }
-     if (chon1 == "usd" && chon2 == "VN"){
-         document.getElementById("result").innerHTML = "Result:  " + money * 23000;
-     }
- }
+    if (chon1 == "usd" && chon2 == "VN") {
+        document.getElementById("result").innerHTML = "Result:  " + money * 23000;
+    }
+}
