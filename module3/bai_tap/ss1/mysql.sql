201656b6190c9f6204853dcc3313589445dbe034
Your Name <you@example.com>
2023-02-08 19:01:41 +0700

    'id'

---
+++ module3/bai_tap/ss1/mysql.sql
@@ -0,0 +1,11 @@
+create database ss1;
+create table class (
+id int primary key,
+name varchar(45)
+);
+create table teacher (
+id int primary key,
+name varchar(45),
+age int,
+country varchar(45)
+);
