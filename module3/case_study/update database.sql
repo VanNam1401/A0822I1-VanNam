edf8748e11de8e5857d0bce2945a1e306e022de2
Your Name <you@example.com>
2023-03-04 15:11:01 +0700

    'id'

--- module3/case_study/update database.sql
+++
@@ -1,7 +0,0 @@
-use casestudy_jsp_servlet;
-select * from nhan_vien
-where ho_ten like 'H%' or ho_ten like 't%' or ho_ten like 'k%' limit 15;
-
-SELECT Year(CURRENT_TIMESTAMP) - Year(ngay_sinh) AS years
-FROM khach_hang
-where ((Year(CURRENT_TIMESTAMP) - Year(ngay_sinh)) between 18 and 50) AND (dia_chi = 'Đà Nẵng' or dia_chi = 'Quảng Trị');
