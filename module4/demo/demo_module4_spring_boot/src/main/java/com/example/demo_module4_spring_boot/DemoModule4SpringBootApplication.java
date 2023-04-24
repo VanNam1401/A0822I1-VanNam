2d1b635a3186988e0ff22346a45d5f9d1646bb80
VanNam1401 <vannamdang2003@gmail.com>
2023-04-25 00:20:42 +0700

    id

--- module4/demo/demo_module4_spring_boot/src/main/java/com/example/demo_module4_spring_boot/DemoModule4SpringBootApplication.java
+++ module4/demo/demo_module4_spring_boot/src/main/java/com/example/demo_module4_spring_boot/DemoModule4SpringBootApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoModule4SpringBootApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoModule4SpringBootApplication.class, args);
     }
--- module4/demo/demo_module4_spring_boot/src/main/java/com/example/demo_module4_spring_boot/repository/IStudentRepository.java
+++ module4/demo/demo_module4_spring_boot/src/main/java/com/example/demo_module4_spring_boot/repository/IStudentRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
+@Repository
 @Transactional
 public interface IStudentRepository extends JpaRepository<Student, Integer> {
-
     @Modifying
     //    @Query(value = "select s from Student s where s.nameStudent like :name")
     @Query(value = "select * from student  where name_student like :name", nativeQuery = true)
